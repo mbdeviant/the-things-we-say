d00fe8cdd4e4d06818b6db0f52f24e45a62d6e90
deviant <mb.deviant@gmail.com>
2024-03-11 23:13:45 +0300

    add messages to index route

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,6 +1,19 @@
 var express = require("express");
 var router = express.Router();
 
+const messages = [
+  {
+    text: "hello there",
+    user: "obi",
+    added: new Date(),
+  },
+  {
+    text: "general kenobi",
+    user: "grievous",
+    added: new Date(),
+  },
+];
+
 /* GET home page. */
 router.get("/", function (req, res, next) {
   res.render("index", { title: "Express" });
