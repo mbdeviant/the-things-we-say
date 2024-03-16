9d0fa1906d561746f616e59a100c67b441f5e7df
deviant <mb.deviant@gmail.com>
2024-03-16 22:52:30 +0300

    edit posting method

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,29 +1,14 @@
 var express = require("express");
 var router = express.Router();
 
-const messages = [
-  {
-    text: "hello there",
-    user: "obi",
-    added: new Date(),
-  },
-  {
-    text: "general kenobi",
-    user: "grievous",
-    added: new Date(),
-  },
-];
+const messages = [];
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
   res.render("index", { title: "Express", messages: messages });
 });
 
-router.get("/new", function (req, res) {
-  res.render("form", { title: "Form" });
-});
-
-router.post("/new", function (req, res, next) {
+router.post("/", function (req, res, next) {
   const message = req.body.message;
   const username = req.body.username;
 
