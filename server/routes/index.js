06432a67233c996e1e7d8c9c58747f8de399bd53
deviant <mb.deviant@gmail.com>
2024-03-19 06:55:45 +0300

    import the function for fetching messages and assign it to a variable

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,7 +1,8 @@
 var express = require("express");
 var router = express.Router();
+const { getMessages } = require("../db");
 
-const messages = [];
+const messages = getMessages();
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
