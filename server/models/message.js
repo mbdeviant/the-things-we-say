acb3dab8467ef4be9aef593c9dfd7877182ff8e4
deviant <mb.deviant@gmail.com>
2024-03-19 08:14:24 +0300

    rename schema item

--- server/models/message.js
+++ server/models/message.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose");
 
 const messageSchema = new mongoose.Schema({
-  message: { type: String, required: true },
   username: { type: String, default: "Anonymous" },
+  text: { type: String, required: true },
   date: { type: Date, required: true },
 });
 
