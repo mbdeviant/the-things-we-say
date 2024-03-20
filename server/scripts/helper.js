d4ae040da18611fa8d51b3e81e87ce3eb144139b
deviant <mb.deviant@gmail.com>
2024-03-20 06:44:28 +0300

    create helper function for form validation

---
+++ server/scripts/helper.js
@@ -0,0 +1,12 @@
+function validate() {
+  const username = document.getElementById("username").value.trim();
+  const message = document.getElementById("message").value.trim();
+
+  if (username == "" || message == "") {
+    alert("Username and message cannot be blank!");
+    return false;
+  }
+  return true;
+}
+
+module.exports = validate;
