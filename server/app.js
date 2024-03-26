37cb3977609c7e0f328a5714043fb2f41f6ee254
deviant <mb.deviant@gmail.com>
2024-03-26 13:57:24 +0300

    remove obsolete path for scripts folder

--- server/app.js
+++ server/app.js
@@ -23,7 +23,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
-app.use("/scripts", express.static(path.join(__dirname, "scripts")));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
