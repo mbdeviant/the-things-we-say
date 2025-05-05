5d8b27a3ccdf1533797d804ed060906235c99441
deviant <mb.deviant@gmail.com>
2025-05-06 02:39:12 +0300

    change rate limit

--- server/app.js
+++ server/app.js
@@ -27,7 +27,7 @@ app.use(
 
 const limiter = rateLimit({
   windowMs: 5 * 60 * 1000,
-  max: 50,
+  max: 110,
   message:
     "Too many requests from this IP, please try again after five minutes",
 });
